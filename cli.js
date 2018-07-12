c40c9a54914d99ece6d28142eec169add79dca1d
Ira Miller <public@iramiller.com>
2018-07-12 12:19:27 -0500

    cache

--- cli.js
+++ cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-// eslint-disable-file no-console
 const program = require('commander')
 const VERSION = require('./package.json').version
+const { cache } = require('guld-ledger') /* , balance */
 
+/* eslint-disable no-console */
 program
   .name('guld-ledger')
   .version(VERSION)
@@ -11,8 +12,26 @@ program
     if (n) process.env.GULDNAME = global.GULDNAME = n
     return true
   })
-  // .option('-q, --quiet', '')
-  .command('source [file]', 'Parse a journal file and checks it for errors.  ledger will return success if no errors are found.')
-//  .command('source [file]', 'Parse a journal file and checks it for errors.  ledger will return success if no errors are found.')
+/*
+  .command('balance [file] [query]')
+  .alias('bal')
+  .description('Reports the current balance of all accounts matching the query.')
+  .action(async function (file = cache.LEDGERPATH, query, options) {
+    balance(file).then(console.log).catch(e => {
+      console.error(e)
+      process.exit(1)
+    })
+  })
+program
+*/
+  .command('cache [path]')
+  .description('Cache all ledger files found (recursively) in the given path or $HOME/ledger.')
+  .action(async function (p = cache.LEDGERDIR, options) {
+    cache.includeJournal(p).then(console.log('Ok.')).catch(e => {
+      console.error(e)
+      process.exit(1)
+    })
+  })
+/* eslint-enable no-console */
 
 program.parse(process.argv)
